1298741cc6af525ab7751991ebcad777bbfaabbf
Satyadev <smoolagani@gmail.com>
2023-07-08 13:39:02 -0400

    Fix

--- realtime-chat-app/src/components/ChatScreen.tsx
+++ realtime-chat-app/src/components/ChatScreen.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextInput from "./TextInput";
 import LeftBubble from "./LeftBubble";
